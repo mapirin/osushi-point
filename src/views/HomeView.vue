14881c0523999df6845405ace15fb5f03a6e372b
mapirin <812matahiro29@gmail.com>
2024-08-09 18:39:25 +0900

    first commit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,49 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<div class="text-center">
+    <h1>{{ appName }}</h1>
+    <div class="mb-3">
+        <p>現在のポイント</p>
+        <p>{{ currentPoint }}</p>
+    </div>
+    <div class="mb-3">
+        <button @click="plus" class="btn btn-primary">ためる</button>
+    </div>
+    <div class="mb-3">
+        <div  v-if="currentPoint < 4">
+            <button @click="minus" class="btn btn-primary" disabled>つかう</button>
+        </div>
+        <div v-else>
+            <button @click="minus" class="btn btn-primary">つかう</button>
+        </div>
+    </div>
+    <div class="p-3 mb-2 bg-success text-white">
+        <p>{{ message }}</p>
+    </div>
+</div>
 </template>
-
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+    name: 'OsushiHome',
+    components: {
+
+    },
+    methods: {
+        plus(){
+            this.message = "1ポイント貯める";
+            this.currentPoint ++;
+        },
+        minus(){
+            this.message = "4ポイント使う";
+            this.currentPoint -= 4;
+        }
+    },
+    data: () => ({
+        appName: "Oshshi Point",
+        currentPoint: "0",
+        message: " " 
+    }),
 }
 </script>
+<style>
+
+</style>
---
+++ src/views/RirekiView.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="text-center">
+        <h1>{{ appName }}</h1>
+        <table border="1">
+            <tr>
+                <th v-for="map in getRirekiMap" v-bind:name="map.id">
+                    {{ map.text }}
+                </th>
+            </tr>
+            <tr v-for="data in getRirekiData">
+                <td v-for="map in getRirekiMap" v-bind:name="map.id">
+                    {{ data[map.id] }}
+                </td>
+            </tr>
+        </table>
+    </div>
+</template>
+<script>
+export default {
+    name: 'Rireki',
+    components: {
+    },
+    methods: {
+        getRirekiMap(){
+            return this.rirekiMap;
+        },
+        getRirekiData(){
+            return this.rirekiData;
+        }
+    },
+    data: () => ({
+        appName: "Oshshi Point Rireki",
+        rirekiMap: [
+            {id:"id", text:"番号"},
+            {id:"type", text:"タイプ"},
+            {id:"date", text:"日付"}
+        ],
+        rirekiData: [
+            {id:"5", type:"つかう", date: "2020/1/4"},
+            {id:"4", type:"ためる", date: "2020/1/4"},
+            {id:"3", type:"ためる", date: "2020/1/3"},
+            {id:"2", type:"ためる", date: "2020/1/2"},
+            {id:"1", type:"ためる", date: "2020/1/1"}
+        ]
+    }),
+}
+</script>
+<style>
+
+</style>
