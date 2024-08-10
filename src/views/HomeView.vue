f81d6ae87d8c8c83c6eaefb43d87aa57a5600f11
mapirin <812matahiro29@gmail.com>
2024-08-10 11:28:21 +0900

    改良

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,7 +6,14 @@
         <p>{{ currentPoint }}</p>
     </div>
     <div class="mb-3">
-        <button @click="plus" class="btn btn-primary">ためる</button>
+        <button @click="openModal" class="btn btn-primary">ためる</button>
+          <div id="overlay" v-show:="showContent">
+            <div id="content">
+              <p>ポイントを貯めますか？</p>
+              <button @click="plus">はい</button>
+              <button @click="closeModal">いいえ</button>
+            </div>
+          </div>
     </div>
     <div class="mb-3">
         <div  v-if="currentPoint < 4">
@@ -16,7 +23,7 @@
             <button @click="minus" class="btn btn-primary">つかう</button>
         </div>
     </div>
-    <div class="p-3 mb-2 bg-success text-white">
+    <div class="p-3 mb-2 text-red">
         <p>{{ message }}</p>
     </div>
 </div>
@@ -28,22 +35,88 @@ export default {
 
     },
     methods: {
+        openModal(){
+          this.showContent = true;
+        },
+        closeModal(){
+          this.showContent = false; 
+        },
         plus(){
-            this.message = "1ポイント貯める";
+            this.message = "おつかれさま！";
             this.currentPoint ++;
         },
         minus(){
-            this.message = "4ポイント使う";
+            this.message = "ご褒美！";
             this.currentPoint -= 4;
         }
     },
     data: () => ({
-        appName: "Oshshi Point",
+        appName: "おすしポイント",
         currentPoint: "0",
-        message: " " 
+        message: " ", 
+        showContent: false
     }),
 }
 </script>
 <style>
+  body{
+    background-image:
+        url(../assets/tamago.png),
+        url(../assets/buri.png),
+        url(../assets/hamachi.png),
+        url(../assets/ikura.png),
+        url(../assets/kinmedai.png),
+        url(../assets/negitoro.png),
+        url(../assets/torotaku.png),
+        url(../assets/salmon.png),
+        url(../assets/tai.png),
+        url(../assets/nodoguro.png),
+        url(../assets/hotate.png),
+        url(../assets/inarizushi.png),
+        url(../assets/iwashi.png),
+        url(../assets/anago.png);
+    background-position:   
+        top 200px left 30px,
+        top 300px left 30px,
+        top 400px left 30px,
+        top 500px left 30px,
+        top 600px left 30px,
+        top 600px left 160px,
+        top 600px left 290px,
+        top 600px left 420px,
+        top 600px left 550px,
+        top 500px left 550px,
+        top 400px left 550px,
+        top 300px left 550px,
+        top 200px left 550px;
+    background-repeat:
+        no-repeat,
+        no-repeat,
+        no-repeat,
+        no-repeat,
+        no-repeat,
+        no-repeat,
+        no-repeat,
+        no-repeat;
+  },
+  #overlay{
+    z-index:1;
 
+    position:fixed;
+    top:0;
+    left:0;
+    width:100%;
+    height:100%;
+    background-color:rgba(0,0,0,0.5);
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  },
+  #content{
+    z-index:2;
+    width:50%;
+    padding: 1em;
+    background:#fff;
+  }
 </style>
